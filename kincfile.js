e8898166c016893e8f4aae8b2a6d201fe26a9930
Lucas <lbergmanns@gmail.com>
2022-04-22 21:58:41 +0200

    Rendering

--- kincfile.js
+++ kincfile.js
@@ -1,6 +1,9 @@
 const project = new Project('Nuklear');
 
 project.addFile('sources/**');
+project.addFile('shaders/**');
 project.addIncludeDir('sources');
+project.setDebugDir('Deployment');
+project.flatten();
 
 resolve(project);
